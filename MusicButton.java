c614c640f10bb8969c212fb06575f98cdbd7bac6
yyyannnn <349168872@gapps.yrdsb.ca>
2024-10-23 12:52:14 -0400

    Added start button

---
+++ MusicButton.java
@@ -0,0 +1,18 @@
+import greenfoot.*;
+
+public class MusicButton extends Button {
+    private Runnable action;
+    public boolean isOff = false;
+
+    public MusicButton (Runnable action) {
+        super(action);
+
+        GreenfootImage musicOn = new GreenfootImage("images/musicOn.png");
+        GreenfootImage musicOff = new GreenfootImage("images/musicOff.png");
+
+        setImage(musicOn);
+    }
+
+    
+
+}
--- TitleScreen.java
+++ TitleScreen.java
@@ -4,14 +4,22 @@ public class TitleScreen extends World {
     public TitleScreen() {
         super(600, 400, 1);
 
-        Label gameLabel = new Label("Game", 100);
+        Label gameLabel = new Label("Hangman", 100);
         addObject(gameLabel, getWidth()/2, 100);
 
-        // Button startButton = new StartButton(this::goInstructions);
-        // addObject(startButton, 300, 340);
+        Button startButton = new StartButton(this::goGameScreen);
+        addObject(startButton, 300, 340);
+
+        Button musicButton = new MusicButton(this::changeVolume);
+        addObject(musicButton, 50, 50);
 
         // Button starButton = new StarButton(this::goHighScore);
         // addObject(starButton, 50, 50);
+    }
+
+
+    public void goGameScreen() {
+        Greenfoot.setWorld(new MyWorld());
     }
 
 
