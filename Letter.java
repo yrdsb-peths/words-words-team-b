2e6298340980b46722b24190d7380e999b2ab734
polo2325 <flipflop2325@outlook.com>
2024-10-24 13:02:21 -0400

    create a letter class

---
+++ Letter.java
@@ -0,0 +1,27 @@
+
+public class Letter extends Actor
+{
+    private char letter;
+    private boolean isGuess = false;
+    private int size = 10;
+    
+    private int xPos;
+    private int yPos;
+    
+    private MyWorld world = (MyWorld) getWorld();
+
+    /**
+     * Constructor for objects of class Letter
+     */
+    public Letter(char theLetter, int theXPos, int theYPos)
+    {
+        letter = theLetter;
+        xPos = theXPos;
+        yPos = theYPos;
+        
+        Label letterLabel = new Label(letter, 10);
+        world.addObject(letterLabel, xPos, yPos);
+    }
+
+   
+}
