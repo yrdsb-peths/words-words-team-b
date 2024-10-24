e1929b1df84ec24964617adfeb75ea852de450df
yyyannnn <349168872@gapps.yrdsb.ca>
2024-10-24 12:36:54 -0400

    Added clicking sound

--- Button.java
+++ Button.java
@@ -3,12 +3,18 @@ import greenfoot.*;
 public class Button extends Actor {
     private Runnable action;
 
+    GreenfootSound clickSound = new GreenfootSound("sounds/mouseClick.mp3");
+
     public Button (Runnable action) {
         this.action = action;
     }
 
     public void act() {
         if (Greenfoot.mouseClicked(this)) {
+            
+            clickSound.play();
+            Greenfoot.delay(20);
+
             if (action != null) {
                 action.run();
             }
