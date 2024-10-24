7df15ab72614fc13509341f252ddacb7dcaf217f
polo2325 <flipflop2325@outlook.com>
2024-10-24 13:26:57 -0400

    delete the guessWord class and move createMap method to MyWorld

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,32 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    private final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+
     public MyWorld() {
         super(600, 400, 1);
+        
+        System.out.println(ALPHABET.length());
+    }
+    
+    //map the alphabet 
+    private HashMap<Character, Letter> createMap(String word)
+    {
+        HashMap<Character, Boolean> secretWord = new HashMap<Character, Boolean>();
+        
+        char[] charArray = word.toCharArray();
+        
+        for(char character : charArray)
+        {
+            if(character == ' ' || character == '-')
+            {
+                /secretWord.put((Character), char new Boolean(false));
+            }
+            
+            secretWord.put((Character) character, new Boolean(false));
+        }
+        
+        return secretWord;
     }
 }
