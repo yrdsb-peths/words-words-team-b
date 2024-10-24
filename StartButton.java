6dbf41262aa1cc09ca6237dc901807b5df468579
yyyannnn <349168872@gapps.yrdsb.ca>
2024-10-24 12:51:03 -0400

    Added comments

--- StartButton.java
+++ StartButton.java
@@ -6,6 +6,7 @@ public class StartButton extends Button {
     public StartButton (Runnable action) {
         super(action);
 
+        // Load and set image 
         GreenfootImage image = new GreenfootImage("images/startButton.png");
         setImage(image);
     }
