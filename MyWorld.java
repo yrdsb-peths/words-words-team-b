926a7dd4fbcc7e20b5ab4da8650536d2d242b6f3
yyyannnn <349168872@gapps.yrdsb.ca>
2024-10-25 13:04:18 -0400

    Added Head

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+
+        Hangman hangmanHead = new HangmanHead();
+        addObject(hangmanHead, 150, 150);
     }
 }
