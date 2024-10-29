fda0587ca330e2d3ca735a3a50891d012d3713c4
khollyli <94080024+khollyli@users.noreply.github.com>
2024-10-29 13:03:04 -0400

    Update GameWorld.java

--- GameWorld.java
+++ GameWorld.java
@@ -101,12 +101,12 @@ public class GameWorld extends World {
                 } else if (incorrect == 6) {
                     HangmanBody rightLeg = new HangmanBody("arm", false);
                     addObject(rightLeg, 580, 430);
-                } else {
+                    
                     // create game end screen
                     isWin = false;
                     EndScreen newScreen = new EndScreen(isWin);
                     Greenfoot.setWorld(newScreen);
-                }
+                } 
             }
         }
     }
