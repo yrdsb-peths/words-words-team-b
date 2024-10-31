28a793be8b65f34ee4216b18093ebac67f2d6417
polo2325 <flipflop2325@outlook.com>
2024-10-31 12:33:04 -0400

    Add next button image

--- NextRoundButton.java
+++ NextRoundButton.java
@@ -11,6 +11,11 @@ public class NextRoundButton extends Button
     public NextRoundButton(Runnable action)
     {
         super(action);
+
+        // Load and set image 
+        GreenfootImage image = new GreenfootImage("images/greenButton.png");
+        image.scale(200, 75);
         
+        setImage(image);
     }
 }
